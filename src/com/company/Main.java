33bb1631aeadf0d328b20e40f6471e2eb9dc87d8
Szymon Iwański <szymon.iwanski@hotmail.com>
2014-12-05 21:32:41 +0100

    It's test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ import static java.lang.System.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    out.println("Hello World!\n");
-        out.println("It's my first commit on GitHub from IntelliJ :)");
+        out.println("Hello World!\n");
+        out.println("It's my first commit");
     }
 }
