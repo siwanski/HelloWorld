1eab86bb8190b025e8c71be1b2cfbddea2f31849
Szymon Iwański <szymon.iwanski@hotmail.com>
2014-12-05 21:21:05 +0100

    It's test

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.io.IOException;
+
+import static java.lang.System.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	    out.println("Hello World!\n");
+    }
+}
