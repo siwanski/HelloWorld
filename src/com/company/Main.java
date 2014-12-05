2114719514b16d62458b80cbf77d8892ccb702a6
Szymon Iwański <szymon.iwanski@hotmail.com>
2014-12-05 21:25:05 +0100

    It's test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,5 +8,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 	    out.println("Hello World!\n");
+        out.println("It's my first commit on GitHub from IntelliJ :)");
     }
 }
